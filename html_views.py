4d14f89d6802bb81eafb5108c16a3e62af4d040e
boboslee6727-Dr <boboslee6727@gmail.com>
2026-07-18 21:43:50 +0900

    Update HTML content in get_intro_html function

--- html_views.py
+++ html_views.py
@@ -180,9 +180,9 @@ def get_styled_td(ganji, oh_class):
 def get_intro_html():
     return """
     <div style='font-size: 16px; font-weight: 600; color: #333; text-align: justify; line-height: 1.8; margin-bottom: 10px; padding: 0 10px;'>
-        <p style='text-indent: 15px; margin: 0 0 5px 0;'><b>"{전통 명리학}"</b>는 1년에 한 번 돌아오는 <b>'{12월지와 60일주}'</b>의 조합으로 <b>'{720개 유형}'</b>으로 분류하지만,</p> 
-        <p style='text-indent: 15px; margin: 0 0 5px 0;'><b>"{시공 명리학}"</b>는 5년에 한 번 돌아오는 <b>'{60월령과 60일주}'</b>의 조합으로 <b>'{3,600개 유형}'</b>으로 분류하여 풀이합니다. </p>
-        <p style='text-indent: 15px; margin: 0 0 5px 0;'><b> 따라서, <b>"{본 초연 시공명리}"</b>는 전통명리에 비해<b>'{5배}'<b>, 요즘 유행하는 <b>'{16개 유형의 MBT}'</b>와 비교하면 무려 <b>'{225배}'</b> 더 세밀화된 정밀한 사주풀이입니다.</p>
+        <p style='text-indent: 15px; margin: 0 0 5px 0;'><b>"초연 시공 명리학"</b>는 5년에 한 번 돌아오는 <b>'60월령과 60일주'</b>의 조합으로 <b>'3,600개 유형'</b>으로 분류하기만, </p>
+        <p style='text-indent: 15px; margin: 0 0 5px 0;'><b>"기존의 전통 명리학"</b>은 1년에 한 번 돌아오는 <b>'12월지와 60일주'</b>의 조합으로 <b>'720개 유형'</b>으로 분류하여 풀이합니다.</p> 
+        <p style='text-indent: 15px; margin: 0 0 5px 0;'><b> 따라서, <b>"본 초연 시공명리"</b>는 전통명리에 비해<b>'5배'<b>, 요즘 유행하는 <b>'16개 유형의 MBT'</b>와 비교하면 무려 <b>'225배'</b> 더 세밀화된 정밀한 사주풀이입니다.</p>
     </div>
     """
 
