75894ea6382bd3daecd7e41244eed11efd23a57e
boboslee6727-Dr <boboslee6727@gmail.com>
2026-08-07 10:13:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -228,7 +228,7 @@ with st.sidebar:
                                 found = True
                                 s_sol_fmt = f"{curr_dt.year}년 {curr_dt.month:02d}월 {curr_dt.day:02d}일"
                                 s_lun_fmt = f"{klc_find.lunarYear}년 {klc_find.lunarMonth:02d}월 {klc_find.lunarDay:02d}일"
-                                st.session_state['rev_success_msg'] = f"✅ 양력 {s_sol_fmt}\n 음력 {s_lun_fmt}"
+                                st.session_state['rev_success_msg'] = f"✅양력{s_sol_fmt}\n 음력{s_lun_fmt}"
                                 st.rerun()
                                 break
                             curr_dt -= dt_mod.timedelta(days=1)
@@ -371,7 +371,7 @@ with st.sidebar:
                                     found = True
                                     s_sol_fmt = f"{curr_dt.year}년 {curr_dt.month:02d}월 {curr_dt.day:02d}일"
                                     s_lun_fmt = f"{klc_find.lunarYear}년 {klc_find.lunarMonth:02d}월 {klc_find.lunarDay:02d}일"
-                                    st.session_state['rev_p_success_msg'] = f"✅ 양력 {s_sol_fmt}\n 음력 {s_lun_fmt}"
+                                    st.session_state['rev_p_success_msg'] = f"✅양력{s_sol_fmt}\n 음력{s_lun_fmt}"
                                     st.rerun()
                                     break
                                 curr_dt -= dt_mod.timedelta(days=1)
