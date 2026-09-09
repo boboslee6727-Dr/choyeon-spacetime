0a851dab16d0d9e2a82bc2be0b52c68dd2f3cb7a
boboslee6727-Dr <boboslee6727@gmail.com>
2026-09-09 19:05:34 +0900

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -284,14 +284,34 @@ else:
         )
         u_product = "1-1. 사주팔자와 운세풀이"
         if main_category == "1. 개인 사주팔자 풀이 (종합)":
-            u_product = st.radio("상세 분석 항목:", ["1-1. 사주팔자와 운세풀이", "1-2. 올 해 (특정 연도) 운세 상세분석", "1-3. 이번 달 (특정 월) 운세 상세분석", "1-4. 이번(특정) 주 및 일 운세 상세분석"], key="sub_category_1", on_change=stop_ai)
+            u_product = st.radio("상세 분석 항목:", [
+                "1-1. 사주팔자와 운세풀이", 
+                "1-2. 올 해의 운세 상세분석", 
+                "1-3. 이번 달 운세 상세분석", 
+                "1-4. 이번 주 및 일 운세 상세분석"
+            ], key="sub_category_1", on_change=stop_ai)
         elif main_category == "2. 테마별 특성화 상담":
-            u_product = st.radio("특성화 상품 선택:", ["2-1. 재물운 특화 분석", "2-2. 연애/결혼운 특화 분석", "2-3. 진학운 특화 분석", "2-4. 직업운 특화 분석", "2-5. 건강운 특화 분석", "2-6. 이사 택일", "2-7. 개업 택일"], key="sub_category_2", on_change=stop_ai)
+            u_product = st.radio("특성화 상품 선택:", [
+                "2-1. 재물운 특화 분석", 
+                "2-2. 연애/결혼운 특화 분석", 
+                "2-3. 진학운 특화 분석", 
+                "2-4. 직업운 특화 분석", 
+                "2-5. 건강운 특화 분석", 
+                "2-6. 이사 택일", 
+                "2-7. 개업 택일"
+        ], key="sub_category_2", on_change=stop_ai)
         elif main_category == "3. 커플 연애/결혼운 (궁합) 풀이":
-            u_product = st.radio("상세 분석 항목:", ["3-1. 연애/결혼운 (궁합) 풀이", "3-2. 결혼 택일", "3-3. 출산 택일"], key="sub_category_3", on_change=stop_ai)
+            u_product = st.radio("상세 분석 항목:", [
+                "3-1. 연애/결혼운 (궁합) 풀이", 
+                "3-2. 결혼 택일", 
+                "3-3. 출산 택일"
+        ], key="sub_category_3", on_change=stop_ai)
         elif main_category == "4. 타 감명서 비교":
-            u_product = st.radio("타 감명서 비교:", ["4-1. 타 감명서 비교 (사주)", "4-2. 타 감명서 비교 (궁합)"], key="sub_category_4", on_change=stop_ai)
-            
+            u_product = st.radio("타 감명서 비교:", [
+                "4-1. 타 감명서 비교 (사주)", 
+                "4-2. 타 감명서 비교 (궁합)"
+            ], key="sub_category_4", on_change=stop_ai)
+                
         st.markdown("---")
 
         if "u_g" not in st.session_state: st.session_state["u_g"] = "남성"
